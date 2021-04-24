84d7c4cfab9b54414e1be77b42e9059c2441adbe
davyzhang3 <davyzhang325@gmail.com>
2021-04-23 23:28:32 -0400

    added waist extender url

--- read_json.py
+++ read_json.py
@@ -2,8 +2,10 @@ import jsonlines
 
 lines = []
 
-with jsonlines.open('search_results_output.jsonl') as myfile:
+with jsonlines.open('revised_search_results_output.jsonl') as myfile:
     for line in myfile.iter():
         lines.append(line)
 
-print(lines[0])
+with open('waist_extender_url.txt','w') as f:
+    for line in lines:
+        f.write('https://www.amazon.com/' + line['url'])
